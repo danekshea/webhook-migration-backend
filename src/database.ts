de2694ccc8bb67fcc12efd373831208d981a83bb
Dane Shea <dane.shea@Danes-MBP.lan>
2024-06-12 14:49:55 +1000

    quick fix

--- src/database.ts
+++ src/database.ts
@@ -19,7 +19,6 @@ export async function addTokenMinted(
     await prisma.tokens.create({
       data: {
         burned,
-        minted,
         burnEVMTransactionHash,
         mintEVMTransactionHash,
         mintUUID,
